fe77efeab7d66fe4c8e08233efec299e619090d9
Lunarlover <91410058+Lunarlover@users.noreply.github.com>
2021-10-03 22:40:40 +0800

    好客山东
    本小程序由五个页面组成，首页是山东的介绍，其他四个分别是泰山、三孔、泉城和自己家乡的介绍。

---
+++ haokeshandong/pages/index/index.js
@@ -0,0 +1,6 @@
+// index.js
+Page({
+  data: {
+    imgsrc:"images/01.jpeg"
+  }
+})
---
+++ haokeshandong/pages/quancheng/quancheng.js
@@ -0,0 +1,33 @@
+// pages/quancheng/quancheng.js
+Page({
+  data: {
+    swiperCurrent: 1,
+    arr: [
+    {images: 'images/01.jpg'},
+    {images: 'images/02.jpg'},
+    {images: 'images/03.webp'}
+  ],
+    indicatorDots:true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    circular: true,
+    beforeColor: "white",//指示点颜色  
+    afterColor: "coral",//当前选中的指示点颜色  
+    previousmargin: '40rpx',//前边距
+    nextmargin: '40rpx',//后边距
+  },
+  
+  //轮播图的切换事件  
+  swiperChange: function (e) {
+    console.log(e.detail.current);
+    this.setData({
+      swiperCurrent: e.detail.current   //获取当前轮播图片的下标
+    })
+  },
+  //滑动图片切换  
+  chuangEvent: function (e) {
+    this.setData({
+      swiperCurrent: e.currentTarget.id
+    })
+  }})
